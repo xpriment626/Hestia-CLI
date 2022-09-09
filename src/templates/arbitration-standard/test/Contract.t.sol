07650ee083c56c9aad13e3c697f89634daf66b61
Emmett Childs <emmettchilds@thegobospot.com>
2022-09-09 13:04:48 +0800

    feat: added template with arbitration standard

---
+++ src/templates/arbitration-standard/test/Contract.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../lib/forge-std/src/Test.sol";
+import "../src/Escrow.sol";
+
+contract ContractTest is Test {
+    function setUp() public {}
+
+    function testMyTest() public {}
+}
